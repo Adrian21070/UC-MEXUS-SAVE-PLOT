359b78af671dae6e6b5417a816e0de0e33d48f0a
rauldds <64034205+rauldds@users.noreply.github.com>
2021-09-08 20:36:30 -0500

    Comments and Read Me

--- PlotAnimation.py
+++ PlotAnimation.py
@@ -1,3 +1,16 @@
+'''
+----------------------------------------------------------
+    @file: PlotAnimation.py
+    @creation_date: Sep 6, 2021
+    @date_modif: Sep 8, 2021
+    @author: Raul Dominguez
+    @e-mail: a01065986@itesm.mx
+    @brief: Script to graph PM measurements of Purple Air 
+    sensors distributed at a certain place and time
+    @Note: Modify the keys and channel IDs accordingly
+----------------------------------------------------------
+'''
+
 #importing libraries
 from datetime import datetime
 from dateutil import tz
@@ -11,32 +24,46 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+#creating lists
 P1_ATM_MULT_VECTOR = []
 time = []
 time_utc = []
-from_zone = tz.tzutc()
-to_zone = tz.tzlocal()
-columns = 3
-rows = 3
-animation_counter = 0
-result_num = 20
-
 keys = ['I6HHDN7MGBXGECUH', 'WD8JU2VCW5WLIGUM',"CPGRHJ0EEFFTFBX5", "LGKHV8ANQZFC1Q84","NCJQ0WH0K6A9FWRP",
         "TK0MF8MQ874GEOMD", "G7RGPVGM9XZG1KIJ", "TU6M4KIO9VGXMGSJ", "OFD7LYESIC6ORDAF"]
 #primario y despues secundario
 channel_ids =[1066387, 958808,1066274,870317, 871042,873018, 978296, 833190,893120]
 
+#creating time related variables
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
 
+#defining the sensor arrangement 
+columns = 3
+rows = 3
+
+animation_counter = 0
+
+#number of measurements retrieved from each sensor
+result_num = 20
+
+#creation of list with the components for the x axis of the plot
 x_axis=(list(range(0,rows))*columns)
 x_axis = [element * 6 for element in x_axis]
+
+#creation of a list with the component for the y axis of the plot
 column_with_interval = np.arange(0,columns*6,6)
 y_axis = np.concatenate([([t]*rows) for t in column_with_interval], axis=0)
 
+#defining plot
 fig, ax = plt.subplots()
-ax1 = plt.axes(projection='3d')
-#creating a subplot 
+ax1 = plt.axes(projection='3d') 
 
 def animate(i):
+    '''
+        @name: animate
+        @brief: function to retrieve the PM of the sensors at a certain time and plot it
+        @return: --
+    '''
     global animation_counter,result_num
     z_axis = []
     shown_time = time[animation_counter]
@@ -51,7 +78,8 @@ def animate(i):
     #ax2.plot3D(x_axis, y_axis, P1_ATM_INDS, 'green');
     ax1.plot_trisurf(x_axis, y_axis, z_axis, cmap='viridis', edgecolor='none');
     plt.title(str(shown_time))	
-	
+
+#loop to retrieve the measurements from sensors	
 for j in range(len(keys)):
     TSobject = Thingspeak(read_api_key=keys[j], channel_id=channel_ids[j])
     data,c= TSobject.read_one_sensor(result=result_num)
@@ -68,6 +96,7 @@ for j in range(len(keys)):
     #print(utcstr)
     P1_ATM_MULT_VECTOR.append(P1_ATM_IND)
 
-
+#function to animate the plot and update it (using the animate function) every certain amount of milliseconds
 ani = animation.FuncAnimation(fig, animate, interval=1000) 
+#print plot
 plt.show()
